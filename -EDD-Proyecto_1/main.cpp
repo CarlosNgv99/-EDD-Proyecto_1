09834eb341918bf4f03ea041bca029467a6fe968
carlosngv99 <carlosngv99@hotmail.com>
2020-06-05 22:12:54 -0600

    Initial Commit

---
+++ -EDD-Proyecto_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  -EDD-Proyecto_1
+//
+//  Created by Carlos NG on 6/5/20.
+//  Copyright © 2020 Carlos NG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
